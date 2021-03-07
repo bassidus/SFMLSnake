e181cd0cfc6476cd60ab526bf755897df5f6c9ce
han/wong <han.wong@pm.me>
2021-03-07 23:35:31 +0100

    new class GameObject

---
+++ SFMLSnake/GameObject.cs
@@ -0,0 +1,20 @@
+﻿using SFML.Graphics;
+using SFML.System;
+
+namespace SFMLSnake
+{
+    public abstract class GameObject
+    {
+        public Location Location;
+        public Sprite Sprite { get; protected set; }
+        public GameObject(Location location)
+        {
+            Location = location;
+        }
+
+        public virtual void Update()
+        {
+
+        }
+    }
+}
