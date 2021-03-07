292f1558909bf692dc1567d20aa34b361f711a4e
han/wong <han.wong@pm.me>
2021-03-07 23:38:09 +0100

    new class Tail

---
+++ SFMLSnake/Tail.cs
@@ -0,0 +1,13 @@
+﻿using SFML.Graphics;
+
+namespace SFMLSnake
+{
+    public class Tail : GameObject
+    {
+        public Tail(Location location) : base(location)
+        {
+            Location = location;
+            Sprite = new Sprite(new Texture("images/green.png"));
+        }
+    }
+}
